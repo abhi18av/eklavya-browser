2f6cbe8b38eba3e6a05c529001afcb87513374ed
Abhinav Sharma <abhi18av@gmail.com>
2018-03-07 19:50:21 +0530

    making progress with the dependency errors

--- project.clj
+++ project.clj
@@ -5,67 +5,64 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [sparkledriver "0.2.2"]
-                 [com.datomic/client-pro "0.8.14"]
+                 [zcaudate/hara "2.8.2"]
+
+;; This inclusion causes many errors in the code
+;                 [zcaudate/lucid "1.4.4"]
+;                 [zcaudate/lucid "1.4.4" :exclusions [org.slf4j/slf4j-log4j] ]
+                 [commons-codec "1.9"]
+
+
+ [com.datomic/client-pro "0.8.14"]
 
 ;; https://github.com/bhauman/rebel-readline
 
                             ;; This is for traversing the nested clojure data structure
-                            [com.rpl/specter "1.0.3"]
+                 [com.rpl/specter "1.0.3"]
 
                             ;; Utility for traversing CLJ(S) data structures
-                            [medley "1.0.0"]
+                 [medley "1.0.0"]
 
                             ; Quickcheck
-                            [org.clojure/test.check "0.10.0-alpha2"]
+                 [org.clojure/test.check "0.10.0-alpha2"]
 
-                           [org.clojure/core.async "0.4.474"]
-;[co.paralleluniverse/pulsar "0.7.7-SNAPSHOT"]
-; https://github.com/tonsky/datascript
-                           [org.clojure/core.logic "0.8.11"]
-[hiccup "1.0.5"]
+                 [org.clojure/core.async "0.4.474"]
 
-                           [org.clojure/core.match "0.3.0-alpha5"]
+                                        ;[co.paralleluniverse/pulsar "0.7.7-SNAPSHOT"]
+                                        ; https://github.com/tonsky/datascript
+                 [org.clojure/core.logic "0.8.11"]
+                 [hiccup "1.0.5"]
 
-
-                            [zcaudate/hara "2.8.2"]
-
-[tick "0.3.5"]
-
-[zcaudate/lucid "1.4.4"]
-
+                 [tick "0.3.5"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
 
 ;; Documentation using marginalia
-[lein-marginalia "0.9.0"]
+                 [lein-marginalia "0.9.0"]
 
 ;; Boot documenataion tool Codox
-[lein-codox "0.10.3"]
+                 [lein-codox "0.10.3"]
 
 ;; Shell library
-[me.raynes/conch "0.8.0"]
+                 [me.raynes/conch "0.8.0"]
 
-[org.clojars.turbopape/milestones "1.0.1"]
-
+                 [org.clojars.turbopape/milestones "1.0.1"]
 
 ;;Logging 
-[com.taoensso/timbre "4.10.0"]
-[com.fzakaria/slf4j-timbre "0.3.8"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.fzakaria/slf4j-timbre "0.3.8"]
 
-[enlive "1.1.6"]
-[spyscope "0.1.6"]
+                 [enlive "1.1.6"]
+                 [spyscope "0.1.6"]
 
 ;; JSON
-[cheshire "5.8.0"]
-
+                 [cheshire "5.8.0"]
 
 ;; PostgreSQL
-[honeysql "0.9.1"]
-[nilenso/honeysql-postgres "0.2.3"]
-
-
-
+                 [honeysql "0.9.1"]
+                 [nilenso/honeysql-postgres "0.2.3"]
 
 ; [org.clojure/core.typed "0.4.3"]
-                 ]
+]
   :main ^:skip-aot eklavya.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/eklavya/edx.clj
+++ src/eklavya/edx.clj
@@ -1,7 +1,8 @@
 (require '[sparkledriver.browser :refer [with-browser make-browser fetch!]])
+
 (require '[sparkledriver.element :refer [find-by-xpath* text]])
 
-(with-browser [browser (make-browser :headless false)]
+(with-browser [browser (make-browser   :headless false)]
   (-> (fetch! browser "http://clojure.org")
       (find-by-xpath* "//div[@class='clj-intro-message']/p")
       (nth 2)
