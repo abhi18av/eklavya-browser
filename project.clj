650ad304d35e89220d7a9a577343a830b164e2df
Abhinav Sharma <abhi18av@gmail.com>
2018-03-07 19:14:51 +0530

    working on the project file

--- project.clj
+++ project.clj
@@ -7,6 +7,64 @@
                  [sparkledriver "0.2.2"]
                  [com.datomic/client-pro "0.8.14"]
 
+;; https://github.com/bhauman/rebel-readline
+
+                            ;; This is for traversing the nested clojure data structure
+                            [com.rpl/specter "1.0.3"]
+
+                            ;; Utility for traversing CLJ(S) data structures
+                            [medley "1.0.0"]
+
+                            ; Quickcheck
+                            [org.clojure/test.check "0.10.0-alpha2"]
+
+                           [org.clojure/core.async "0.4.474"]
+;[co.paralleluniverse/pulsar "0.7.7-SNAPSHOT"]
+; https://github.com/tonsky/datascript
+                           [org.clojure/core.logic "0.8.11"]
+[hiccup "1.0.5"]
+
+                           [org.clojure/core.match "0.3.0-alpha5"]
+
+
+                            [zcaudate/hara "2.8.2"]
+
+[tick "0.3.5"]
+
+[zcaudate/lucid "1.4.4"]
+
+
+;; Documentation using marginalia
+[lein-marginalia "0.9.0"]
+
+;; Boot documenataion tool Codox
+[lein-codox "0.10.3"]
+
+;; Shell library
+[me.raynes/conch "0.8.0"]
+
+[org.clojars.turbopape/milestones "1.0.1"]
+
+
+;;Logging 
+[com.taoensso/timbre "4.10.0"]
+[com.fzakaria/slf4j-timbre "0.3.8"]
+
+[enlive "1.1.6"]
+[spyscope "0.1.6"]
+
+;; JSON
+[cheshire "5.8.0"]
+
+
+;; PostgreSQL
+[honeysql "0.9.1"]
+[nilenso/honeysql-postgres "0.2.3"]
+
+
+
+
+; [org.clojure/core.typed "0.4.3"]
                  ]
   :main ^:skip-aot eklavya.core
   :target-path "target/%s"
