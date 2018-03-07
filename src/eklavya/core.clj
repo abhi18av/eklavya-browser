308b9e295e45f6d67a45aed4f9dae0c6f7ef9339
Abhinav Sharma <abhi18av@gmail.com>
2018-03-07 17:47:56 +0530

    initial commit

---
+++ src/eklavya/core.clj
@@ -0,0 +1,7 @@
+(ns eklavya.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/eklavya/core_test.clj
@@ -0,0 +1,7 @@
+(ns eklavya.core-test
+  (:require [clojure.test :refer :all]
+            [eklavya.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
